0563d988c1aff704cb34a9e64711306e65870133
cvhlpont <pontchvl@gmail.com>
2024-04-11 21:44:18 +0200

    update: removed unused code

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,20 +1,11 @@
 import express from "express";
 const router = express.Router();
 
-import {
-  registerUser,
-  loginUser,
-  getUserProfile,
-  updateUserProfile,
-} from "../models/userModel.js";
+import { registerUser, loginUser } from "../models/userModel.js";
 import { verifyToken } from "../middleware/authMiddleware.js";
 
 // Routes
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
-// // TODO: protect
-// router.get("/profile", verifyToken, getUserProfile);
-// router.put("/profile", verifyToken, updateUserProfile);
 
 export default router;
